82db9e76da193606366da9995101a1474a1b28e1
ruz020 <zhangrui9095@gmail.com>
2018-01-07 23:15:12 -0800

    Update GraphicsGrid.java

--- PR4/GraphicsGrid.java
+++ PR4/GraphicsGrid.java
@@ -46,13 +46,13 @@ public class GraphicsGrid extends JPanel
 		int relHeight = (frameHeight - heightInt)/2;
 		try
 		{
-	        for (int i = 1; i< trackSnake.size(); i++) {       
+			for (int i = 1; i< trackSnake.size(); i++) {       
 				Coord coord = trackSnake.get(i);
 				int cellX = (coord.getR() * pixels) + relWidth;
 				int cellY = (coord.getC() * pixels) + relHeight;
 				g.setColor(Color.GREEN);
 				g.fillRect(cellX, cellY, pixels, pixels);
-	  	     }   
+			}   
 			int cellR = (trackSnake.get(0).getR() * pixels) + relWidth;
 			int cellC = (trackSnake.get(0).getC() * pixels) + relHeight;			
 			g.setColor(Color.RED);
@@ -65,22 +65,20 @@ public class GraphicsGrid extends JPanel
 				g.setColor(Color.BLUE);
 				g.fillRect(cellX, cellY, pixels, pixels);
 			}
-		} catch(Exception e ){}
+		} 
+		catch(Exception e ){}
 
 		g.setColor(Color.BLACK);
 		g.drawRect(relWidth, relHeight, pixels*(int)Math.floor(this.width/this.pixels), pixels*(int)Math.floor(this.height/this.pixels));
 
 		for (int i = 0; i < pixels*(int)Math.floor(this.width/this.pixels); i += pixels) 
 		{
-		   g.drawLine(i + relWidth, relHeight, i + relWidth, 
-           pixels*(int)Math.floor(this.height/this.pixels) + relHeight);
+		   g.drawLine(i + relWidth, relHeight, i + relWidth, pixels*(int)Math.floor(this.height/this.pixels) + relHeight);
 		}
 
 		for (int i = 0; i < pixels*(int)Math.floor(this.height/this.pixels); i += pixels) 
 		{
-		   g.drawLine( relWidth, i + relHeight, 
-           pixels*(int)Math.floor(this.width/this.pixels) + relWidth,
-           i + relHeight);
+		   g.drawLine( relWidth, i + relHeight, pixels*(int)Math.floor(this.width/this.pixels) + relWidth,i + relHeight);
 		}
 
 	}
