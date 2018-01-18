53947b1950914384992ce870dbda86fb8692bbb7
ruz020 <zhangrui9095@gmail.com>
2018-01-17 19:50:59 -0800

    Update Snake.java

--- PR4/Snake.java
+++ PR4/Snake.java
@@ -15,7 +15,7 @@ public class Snake{
 	 * set the initial head coordinate
 	 * set the snakeArray with only one head
 	 */ 
-	public Snake ( Coord initial )
+	public Snake (Coord initial, int maxlength)
 	{
 		head = new Coord (initial);
 		snakeArray.add(head);		
